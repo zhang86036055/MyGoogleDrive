bc012180f9ff28aaf615e12fc5336c398b934cb2
Simon Zhang <szhang2378@gmail.com>
2023-05-16 18:51:26 -0500

    first commit

---
+++ MyGoogleDrive/MyGoogleDrive/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGoogleDrive
+//
+//  Created by Shuai Zhang on 5/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+    }
+
+
+}
+
